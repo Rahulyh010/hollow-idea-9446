27d7e222222835d591a31dad4ffc08edd43d3d95
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-02-21 12:54:13 +0530

    Main

---
+++ trend/src/Routes/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from '../Pages/Home'
+
+export const AllRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+        </Routes>
+    </div>
+  )
+}
