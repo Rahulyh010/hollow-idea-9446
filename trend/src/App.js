766661da2ea52f4bd697246f3d68e78943998cfd
Faizan8092 <ali.faizan2015@gmail.com>
2023-02-22 17:30:44 +0530

    AdminPanel added

--- trend/src/App.js
+++ trend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Admin from './Pages/Admin';
 import { AllRoutes } from './Routes/AllRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <AllRoutes/>
+    <div className="App" style={{margin:"0px",padding:"0px"}}>
+      {/* <AllRoutes/> */}
+      <Admin/>
     </div>
   );
 }
