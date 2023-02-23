470a1890b72263afb53de87d8d78640814d5dc19
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-02-23 22:47:31 +0530

    commit

---
+++ trend/src/Components/footer.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import "../Styles/Footer.css"
+
+export const Footer = () => {
+  return (
+    <div className='foot-main1' >
+
+        {/* 
+        
+     
+
+
+
+
+
+        */}
+
+<div className='footer-main'>
+    <div>
+<h3> Ajio</h3>
+<p>Who We Are</p>
+<p>Join Our Team</p>
+<p>Terms & Conditions</p>
+<p>We Respect Your Privacy</p>
+<p>Fees & Payments</p>
+<p>Returns & Refunds</p>
+<p> Policy</p>
+<p>Promotions Terms & Conditions</p>
+
+    </div>
+    <div>
+        <h3>Help</h3>
+        <p>Track Your Order</p>
+        <p>Frequently Asked Questions</p>
+        <p>ReturnsCancellations</p>
+        <p>Payments</p>
+        <p>Customer Care</p>
+        <p>How Do I Redeem My Coupon</p>
+       
+        
+    </div>
+    <div>
+        <h3> Shop by</h3>
+<p>Men</p>
+<p>Women</p>
+<p>Kids</p>
+<p>IndieStores</p>
+<p>New Arrivals</p>
+<p>Brand Directory</p>
+<p>HomeCollections</p>
+    </div>
+    <div>
+        <h3> Follow us</h3>
+        <p>Facebook</p>
+        <p>Instagram- AJIOlife</p>
+        <p>Instagram- AJIO LUXE</p>
+        <p>Twitter</p>
+        <p>Pinterest</p>
+    </div>
+</div>
+<h3>Follow us</h3>
+<div className='foot1' >
+   
+    <div>
+<div>
+    <img  src="https://cdn-icons-png.flaticon.com/128/2504/2504923.png" alt="" />
+</div>
+<div>
+    <img src="https://cdn-icons-png.flaticon.com/128/1409/1409946.png" alt="" />
+</div>
+<div>
+    <img src="https://cdn-icons-png.flaticon.com/128/2504/2504947.png" alt="" />
+</div>
+<div>
+<img src="https://cdn-icons-png.flaticon.com/128/2504/2504965.png" alt="" />
+</div>
+<div>
+    <img src="https://cdn-icons-png.flaticon.com/128/300/300221.png" alt="" />
+</div>
+    </div>
+
+    <div></div>
+</div>
+
+    </div>
+  )
+}
