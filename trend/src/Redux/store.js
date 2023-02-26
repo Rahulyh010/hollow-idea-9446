2883dc945a6ec759b370a229434a32b51a1cd0cf
AvinashKalmegh <avinashkalmegh93@gmail.com>
2023-02-26 10:59:56 +0530

    add login

--- trend/src/Redux/store.js
+++ trend/src/Redux/store.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import {reducer as product} from "./ProductReducer/reducer"
-
+import {reducer as AuthReducer} from "./AuthReducer/reducer";
 
 
 
 
 const rootReducer=combineReducers({
     product,
-   
+   AuthReducer
 })
 
 
