baaf67c5967d81415f26dfb54312db5530187c8c
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-02-26 19:47:02 +0530

    push

---
+++ trend/src/Components/CartItem.jsx
@@ -0,0 +1,11 @@
+
+import "./cartitem.css"
+function CartItem({img,title,rate,price,handleremove}){
+
+    return <div>
+        <img src="" alt="" />
+        <p></p>
+        <p></p>
+    <button>Remove</button>
+    </div>
+}
--- trend/src/Pages/CartPage.jsx
+++ trend/src/Pages/CartPage.jsx
@@ -3,93 +3,50 @@ import axios from 'axios';
 import styled from "styled-components"
  import "./Cart.css"
 import { Link } from 'react-router-dom';
+import { Navbar } from '../Components/Navbar';
 
 const CartPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
 
-  useEffect(() => {
-    const fetchCart = async () => {
-      const { data } = await axios.get("https://fakestoreapi.com/products");
-      setCart(data);
-      setTotalPrice(data.reduce((acc, item) => acc + (item.quantity * item.price), 0));
-    };
-    fetchCart();
-  }, []);
+  const [cartItems,setCartItems]=useState([])
 
-  const handleQuantityChange = (event, itemId) => {
-    const newCart = cart.map(item => {
-      if (item.id === itemId) {
-        item.quantity = parseInt(event.target.value);
-      }
-      return item;
-    });
-    setCart(newCart);
-    setTotalPrice(newCart.reduce((acc, item) => acc + (item.quantity * item.price), 0));
-  };
+  const getCartItems=()=>{
 
-  const handleRemoveFromCart = (itemId) => {
-    const newCart = cart.filter(item => item.id !== itemId);
-    setCart(newCart);
-    setTotalPrice(newCart.reduce((acc, item) => acc + (item.quantity * item.price), 0));
-  };
+    axios.get(`hh`)
+    .then((res)=>{
+      console.log(res)
+      setCartItems([...cartItems,res.data])
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
 
-  return (
-    <div className="cart-page">
-      <div>
+useEffect(()=>{
 
-      </div>
-      <div>
-        <img src="https://assets.ajio.com/cms/AJIO/WEB/28032021-D-cartpagebanner-relianceones.jpg" alt="" />
-      </div>
-      <h1>My Cart</h1>
-      {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <>
-          <ul className="cart-items" >
-            {cart.map(item => (
-              <div key={item.id} style={{border: "1px solid",padding:"20px"}}>
-                <img style={{width:"50px"}}src={item.image} alt={item.name} />
-                <div className="item-details">
-                  <h2>{item.name}</h2>
-                  <p>{item.price}</p>
-                  <div className="quantity">
-                    <label htmlFor={`quantity-${item.id}`}>Quantity:</label>
-                    <input type="number" id={`quantity-${item.id}`} name={`quantity-${item.id}`} min="1" value={item.quantity} onChange={(e) => handleQuantityChange(e, item.id)} />
-                  </div>
-                  <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
-                </div>
-              </div>
-            ))}
-          </ul>
-          <p className="total-price">Total Price: {totalPrice}</p>
-          <button className="checkout-button">Proceed to Checkout</button>
-        </>
-      )}
-      <div>
-      <Button>ADD FROM WHISLIST</Button>
-      </div>
-     <div style={{color:'blue',marginTop:'20px',fontWeight:'bold'}} >
-      CONTINUE SHOPPING
-     </div>
-     <div>
-      
-     </div>
-      
-      <Link to="/checkout">Checkout</Link>
-    </div>
-  );
-};
+getCartItems()
 
+},[])
 
-const Button=styled.button`
-width:180px;
-height:50px;
-border-radius:10px;
-background-color:orange
-`
+ 
+ return <div>
+<div style={{width:"100%",height:"118px"}} >
 
+</div>
+<Navbar />
+
+<div style={{width:"100%",height:"118px"}}>
+
+</div>
+
+<div id="cart-div" >
+{cartItems.map((e)=>{
+ return <cartItems key={e.id} {...e} />
+})}
+</div>
+
+ </div>
+              
+}
 
 
 
